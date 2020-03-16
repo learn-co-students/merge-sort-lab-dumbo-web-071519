246788086a636fe18083b1873ee325d90e1c5a34
github cjl248 <github cjl248@cornell.edu>
2020-03-16 17:15:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+// example
+// arr1 = [3, 1, 10, 4]
+// [3] [1] [10] [4]
+// [1, 3] [4, 10]
+// [1, 3, 4, 10]
 
+const findMinAndRemoveSorted = (array) => {
+  const smallestNum = array[0]
+  return array.shift()
+
+}
+
+const merge = (firstHalf, secondHalf) => {
+  let mergedArray = []
+  while (firstHalf.length !== 0 && secondHalf.length !== 0) {
+    if (firstHalf[0] < secondHalf[0]) {
+      mergedArray.push(findMinAndRemoveSorted(firstHalf))
+    } else {
+      mergedArray.push(findMinAndRemoveSorted(secondHalf))
+    }
+  }
+  return mergedArray.concat(firstHalf).concat(secondHalf)
+}
+
+const mergeSort = (array) => {
+  let midpoint = array.length/2
+  let firstHalf = array.slice(0, midpoint)
+  let secondHalf = array.slice(midpoint, array.length)
+
+  if (array.length < 2 ) {
+    return array
+  } else {
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  }
+}
